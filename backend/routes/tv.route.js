b31ecf3e57b7f8e63f6339306de304d03bfa382d
Yudip Rai <yudip11@gmail.com>
2025-04-26 14:32:54 +0545

    done

--- backend/routes/tv.route.js
+++ backend/routes/tv.route.js
@@ -13,6 +13,6 @@ router.get("/trending", getTrendingTv);
 router.get("/:id/trailers", getTvTrailers);
 router.get("/:id/details", getTvDetails);
 router.get("/:id/similar", getSimilarTvs);
-// router.get("/category/:category", getTvsByCategory);
+router.get("/category/:category", getTvsByCategory);
 
 export default router;
